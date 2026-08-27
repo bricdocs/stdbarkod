384a778a426f1bcba9a954b8ad3fac37b71c667b
bricdocs <bricdocs.web@gmail.com>
2026-08-27 21:01:05 +0300

    Add binary data for card names and codes
    This file contains a dictionary of binary codes and their corresponding card names for a card game.

---
+++ binary-data.js
@@ -0,0 +1,17 @@
+/* 
+==================================================================================
+KART TANIMA SÖZLÜĞÜ (binary-data.js)
+----------------------------------------------------------------------------------
+Bu dosya ana okuyucu motorundan bağımsız olarak tüm oyun kartlarının binary 
+kodlarını ve isim eşleşmelerini tutar.
+==================================================================================
+*/
+
+const CARD_DATABASE = {
+    "1010110100101": "Karo 9",
+    "1010010010101": "Kupa 5",
+    "1010010110101": "Kupa J (Vale)",
+    "1010011001001": "Maça 3",
+    "1010101001001": "Maça A (As)",
+    "1010100110101": "Karo Q (Kız)" // Önceki testlerimizde bulduğumuz Kız kartı
+};
